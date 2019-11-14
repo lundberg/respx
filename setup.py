9a350a5a50672b4ef55f2b7a2f218c9d5d30ed3f
Jonas Lundberg <jonas@5monkeys.se>
2019-11-14 11:03:03 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 from setuptools import setup
 
 
-VERSION = (0, 1, 0, "final", 0)
+VERSION = (0, 2, 0, "final", 0)
 
 
 def get_version(version=None):
@@ -80,7 +80,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",

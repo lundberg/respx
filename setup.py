c908317a32d6fb1957e5cacaf2aece62882aaf64
Jonas Lundberg <jonas@5monkeys.se>
2020-09-23 00:27:53 +0200

    Require httpx 0.15

--- setup.py
+++ setup.py
@@ -32,5 +32,5 @@ setup(
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.6",
-    install_requires=["httpx>=0.14,<0.15"],
+    install_requires=["httpx>=0.15,<0.16"],
 )

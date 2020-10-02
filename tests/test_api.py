e8d4eaa6331786dd97c7760ff868c6af228a0306
Slava Skvortsov <kondor1995@mail.ru>
2020-10-02 21:26:59 +0200

    Black changes for tests (black sucks[1])

--- tests/test_api.py
+++ tests/test_api.py
@@ -359,21 +359,45 @@ async def test_add(client, method_str, client_method_attr):
 @pytest.mark.parametrize(
     "url_parts, url_model, expected_matches",
     [
-        ((b'http', b'foo.bar', 80, b''), URLPattern('http://foo.bar'), True),
-        ((b'http', b'foo.bar', 80, b'/'), URLPattern('http://foo.bar'), True),
-        ((b'http', b'foo.bar', 80, b'/'), URLPattern('http://foo.bar/'), True),
-        ((b'http', b'foo.bar', 80, b''), URLPattern('http://foo.bar/'), True),
-        ((b'http', b'foo.bar', 80, b'/baz'), URLPattern('http://foo.bar'), False),
-        ((b'https', b'foo.bar', 443, b''), URLPattern('http://foo.bar'), False),
-        ((b'https', b'foo.bar', 443, b''), URLPattern('https://foo.bar'), True),
-        ((b'http', b'foo.bar', 80, b'?baz=1'), URLPattern('http://foo.bar'), True),
-        ((b'http', b'foo.bar', 80, b'?baz=1'), URLPattern('http://foo.bar/?baz=1'), True),
-        ((b'http', b'foo.bar', 80, b'/?baz=1'), URLPattern('http://foo.bar?baz=1'), True),
-        ((b'http', b'foo.bar', 80, b'?baz=1'), URLPattern('http://foo.bar?baz=2'), False),
-        ((b'http', b'foo.bar', 80, b'?baz=1'), URLPattern('http://foo.bar?baz=1&qux=2'), False),
-        ((b'http', b'foo.bar', 80, b'/path?qux=2&baz=1'), URLPattern('http://foo.bar/path?baz=1&qux=2'), True),
+        ((b"http", b"foo.bar", 80, b""), URLPattern("http://foo.bar"), True),
+        ((b"http", b"foo.bar", 80, b"/"), URLPattern("http://foo.bar"), True),
+        ((b"http", b"foo.bar", 80, b"/"), URLPattern("http://foo.bar/"), True),
+        ((b"http", b"foo.bar", 80, b""), URLPattern("http://foo.bar/"), True),
+        ((b"http", b"foo.bar", 80, b"/baz"), URLPattern("http://foo.bar"), False),
+        ((b"https", b"foo.bar", 443, b""), URLPattern("http://foo.bar"), False),
+        ((b"https", b"foo.bar", 443, b""), URLPattern("https://foo.bar"), True),
+        ((b"http", b"foo.bar", 80, b"?baz=1"), URLPattern("http://foo.bar"), True),
+        (
+            (b"http", b"foo.bar", 80, b"?baz=1"),
+            URLPattern("http://foo.bar/?baz=1"),
+            True,
+        ),
+        (
+            (b"http", b"foo.bar", 80, b"/?baz=1"),
+            URLPattern("http://foo.bar?baz=1"),
+            True,
+        ),
+        (
+            (b"http", b"foo.bar", 80, b"?baz=1"),
+            URLPattern("http://foo.bar?baz=2"),
+            False,
+        ),
+        (
+            (b"http", b"foo.bar", 80, b"?baz=1"),
+            URLPattern("http://foo.bar?baz=1&qux=2"),
+            False,
+        ),
+        (
+            (b"http", b"foo.bar", 80, b"/path?qux=2&baz=1"),
+            URLPattern("http://foo.bar/path?baz=1&qux=2"),
+            True,
+        ),
     ],
 )
-def test_url_model_matches(url_parts: URL, url_model: URLPattern, expected_matches: bool) -> None:
+def test_url_model_matches(
+    url_parts: URL,
+    url_model: URLPattern,
+    expected_matches: bool,
+) -> None:
     matches, _ = url_model.matches(url_parts)
     assert matches == expected_matches

f40e2505a5fd7999a0d89c0bc2110f81bbef17d6
Jair Henrique <jair.henrique@gmail.com>
2024-12-19 08:19:16 -0300

    Add support to python 3.13

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ nox.options.reuse_existing_virtualenvs = True
 nox.options.keywords = "test + mypy"
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
 def test(session):
     deps = ["pytest", "pytest-asyncio", "pytest-cov", "trio", "starlette", "flask"]
     session.install("--upgrade", *deps)
